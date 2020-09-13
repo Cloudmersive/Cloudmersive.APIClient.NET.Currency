fc4c0e2cd926102064fd35bd75c2f16a662d9062
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2020-09-12 21:50:18 -0700

    Added

--- build.ps1
+++ build.ps1
@@ -1,5 +1,5 @@
 ﻿Remove-Item –path ./client –recurse
-& java -jar swagger-codegen-cli-2.4.5.jar generate -i https://api.cloudmersive.com/swagger/api/validate -l csharp -o client -c packageconfig.json
+& java -jar swagger-codegen-cli-2.4.5.jar generate -i https://api.cloudmersive.com/swagger/api/currency -l csharp -o client -c packageconfig.json
 #(Get-Content ./client/src/api/ConvertDocumentApi.js).replace('var returnType = Object;', "var returnType = 'Blob';") | Set-Content ./client/src/api/ConvertDocumentApi.js
 #(Get-Content ./client/src/api/ConvertWebApi.js).replace('var returnType = Object;', "var returnType = 'Blob';") | Set-Content ./client/src/api/ConvertWebApi.js
 #& npm build ./client
@@ -8,18 +8,18 @@
 
 
 
-$csprojpath = Resolve-Path ./client/src/Cloudmersive.APIClient.NET.Validate/Cloudmersive.APIClient.NET.Validate.csproj
-$csprojtestpath = Resolve-Path ./client/src/Cloudmersive.APIClient.NET.Validate.Test/Cloudmersive.APIClient.NET.Validate.Test.csproj
-$nuspecpath = Resolve-Path ./client/src/Cloudmersive.APIClient.NET.Validate/Cloudmersive.APIClient.NET.Validate.nuspec
-$slnpath = Resolve-Path ./client/Cloudmersive.APIClient.NET.Validate.sln
+$csprojpath = Resolve-Path ./client/src/Cloudmersive.APIClient.NET.Currency/Cloudmersive.APIClient.NET.Currency.csproj
+$csprojtestpath = Resolve-Path ./client/src/Cloudmersive.APIClient.NET.Currency.Test/Cloudmersive.APIClient.NET.Currency.Test.csproj
+$nuspecpath = Resolve-Path ./client/src/Cloudmersive.APIClient.NET.Currency/Cloudmersive.APIClient.NET.Currency.nuspec
+$slnpath = Resolve-Path ./client/Cloudmersive.APIClient.NET.Currency.sln
 
 
-(Get-Content $nuspecpath).replace('<title>Swagger Library</title>', "<title>Cloudmersive Validate API Client</title><licenseUrl>https://www.apache.org/licenses/LICENSE-2.0.txt</licenseUrl>") | Set-Content $nuspecpath
+(Get-Content $nuspecpath).replace('<title>Swagger Library</title>', "<title>Cloudmersive Currency API Client</title><licenseUrl>https://www.apache.org/licenses/LICENSE-2.0.txt</licenseUrl>") | Set-Content $nuspecpath
 (Get-Content $nuspecpath).replace('<authors>$author$</authors>', "<authors>Cloudmersive</authors>") | Set-Content $nuspecpath
 (Get-Content $nuspecpath).replace('<owners>$author$</owners>', "<owners>Cloudmersive</owners>") | Set-Content $nuspecpath
-(Get-Content $nuspecpath).replace('<description>A library generated from a Swagger doc</description>', "<description>The validation APIs help you validate data. Check if an E-mail address is real. Check if a domain is real. Check up on an IP address, and even where it is located. All this and much more is available in the validation API.</description>") | Set-Content $nuspecpath
+(Get-Content $nuspecpath).replace('<description>A library generated from a Swagger doc</description>', "<description>The currency APIs help you retrieve exchange rates and convert prices between currencies easily.</description>") | Set-Content $nuspecpath
 (Get-Content $nuspecpath).replace('<!-- Authors contain text that appears directly on the gallery -->', "<iconUrl>https://cloudmersive.com/images/cmsdk.png</iconUrl>") | Set-Content $nuspecpath
-(Get-Content $nuspecpath).replace('<dependencies>', "<projectUrl>https://cloudmersive.com/validate-api</projectUrl><dependencies>") | Set-Content $nuspecpath
+(Get-Content $nuspecpath).replace('<dependencies>', "<projectUrl>https://cloudmersive.com/currency-api</projectUrl><dependencies>") | Set-Content $nuspecpath
 
 
 
